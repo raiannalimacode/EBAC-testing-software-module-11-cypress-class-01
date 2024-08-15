8612a7572d5732e92b612d86fbe1a70cef985c15
Raianna Lima <raiannacodelima@gmail.com>
2024-08-15 18:45:25 -0300

    Feat: Test page products.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,16 @@
 /// <reference types="cypress" />
 
 context('Functionality Login', () => {
-  it('Deve fazer login com sucesso', () => {
+
+  beforeEach(() => {
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+  });
+
+  afterEach(() => {
+    cy.screenshot()
+  });
+
+  it('Deve fazer login com sucesso', () => {
     cy.get('#username').type('aluno_ebac@teste.com')
     cy.get('#password').type('teste@teste.com')
     cy.get('.woocommerce-form > .button').click()
@@ -12,7 +20,6 @@ context('Functionality Login', () => {
   })
 
   it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     cy.get('#username').type('ebac@teste.com')
     cy.get('#password').type('teste@teste.com')
     cy.get('.woocommerce-form > .button').click()
@@ -21,7 +28,6 @@ context('Functionality Login', () => {
   })
 
   it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     cy.get('#username').type('aluno_ebac@teste.com')
     cy.get('#password').type('teste.com')
     cy.get('.woocommerce-form > .button').click()
